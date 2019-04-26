5e90afadf4b49343b6c3654a847ad5a6aeb2cba9
DaShinja <byron@dashinja.com>
2019-04-26 15:37:39 -0400

    es6 module not working

--- assets/javascript/link-items.js
+++ assets/javascript/link-items.js
@@ -1,6 +1,49 @@
 // export NOT_BCS_LINKS = ""
 
-export const BCS_LINKS = [
+// export const BCS_LINKS = [
+//   {
+//     a: {
+//       href: 'https://dashinja.github.io/recipeciprocity',
+//       fancybox: 'bcs',
+//     },
+//     img: {
+//       style: '',
+//       src: './assets/images/recipeciprocity.png',
+//       alt: 'Recipeciprocity Hero Image',
+//     },
+//     banner: 'Recipeciprocity',
+//   },
+// ]
+import images from '../images'
+
+export const LINKS_PROJECTS = [
+  {
+    banner: 'Maid Bot',
+    a: {
+      href: 'https://maid-bots.herokuapp.com/',
+      fancybox: 'no-bcs',
+    },
+    img: {
+      style: '',
+      src: images.maidbot,
+      alt: 'Maid Bot Preview Image',
+    },
+  },
+  {
+    banner: 'Notes Node',
+    a: {
+      href: 'https://dashinja.github.io/notes-node',
+      fancybox: 'bcs',
+    },
+    img: {
+      style: '',
+      src: images.crayon,
+      alt: 'Notes Node Crayon Drawing',
+    },
+  },
+]
+
+export const BCS_LINKS_PROJECTS = [
   {
     a: {
       href: 'https://dashinja.github.io/recipeciprocity',
@@ -8,9 +51,132 @@ export const BCS_LINKS = [
     },
     img: {
       style: '',
-      src: './assets/images/recipeciprocity.png',
+      src: images.recipeciprocity,
       alt: 'Recipeciprocity Hero Image',
     },
     banner: 'Recipeciprocity',
+  },
+  {
+    a: {
+      href: 'https://puffroi.herokuapp.com',
+      fancybox: 'bcs',
+    },
+    img: {
+      style: '',
+      src: images.cityPano,
+      alt: 'Circular Rotating City Image',
+    },
+    banner: 'COL-lege',
+  },
+  {
+    a: {
+      href: 'https://demo-lisa.xyz/',
+      fancybox: 'bcs',
+    },
+    img: {
+      style: '',
+      src: images.lisa,
+      alt: 'LISA Business Intelligence Logo',
+    },
+    banner: 'L.I.S.A. Preview',
+  },
+]
+
+export const BCS_LINKS = [
+  {
+    banner: 'Train Scheduler Deluxe',
+    a: {
+      href: 'https://dashinja.github.io/trainSchedulerDeluxe/',
+      fancybox: 'bcs',
+    },
+    img: {
+      style: '',
+      src: images.train,
+      alt: 'Nice Train Profile',
+    },
+  },
+  {
+    banner: 'Liri Bot',
+    a: {
+      href: 'https://dashinja.github.io/liri/',
+      fancybox: 'bcs',
+    },
+    img: {
+      style: '',
+      src: images.liri,
+      alt: 'Liri Bot',
+    },
+  },
+  {
+    banner: 'Las Horcas',
+    a: {
+      href: 'https://dashinja.github.io/wordguessgame',
+      fancybox: 'bcs',
+    },
+    img: {
+      style: '',
+      src: images.wordGuess,
+      alt: 'Black and White Gallows',
+    },
+  },
+  {
+    banner: 'Bamazonathon!',
+    a: {
+      href: 'https://dashinja.github.io/bamazonathon/',
+      fancybox: 'bcs',
+    },
+    img: {
+      style: '',
+      src: images.bamazonathon,
+      alt: 'Yawning Jaguar',
+    },
+  },
+  {
+    banner: 'GIF Haven',
+    a: {
+      href: 'https://dashinja.github.io/giphy/',
+      fancybox: 'bcs',
+    },
+    img: {
+      style: '',
+      src: images.giphy,
+      alt: 'Funny Giphy Placeholder',
+    },
+  },
+  {
+    banner: 'Friend Fest Finder',
+    a: {
+      href: 'https://friendfestfinder.herokuapp.com',
+      fancybox: 'bcs',
+    },
+    img: {
+      style: '',
+      src: images.couple,
+      alt: 'Two Blue Hand Holding Figures',
+    },
+  },
+  {
+    banner: 'Them Burgerz Tho!',
+    a: {
+      href: 'https://dashinja.github.io/themburgerztho',
+      fancybox: 'bcs',
+    },
+    img: {
+      style: '',
+      src: images.burger,
+      alt: 'Juicy Burger',
+    },
+  },
+  {
+    banner: 'Dota2News',
+    a: {
+      href: 'https://dota2news.herokuapp.com',
+      fancybox: 'bcs',
+    },
+    img: {
+      style: '',
+      src: images.dota2,
+      alt: 'Team Liquid Logo',
+    },
   },
 ]
